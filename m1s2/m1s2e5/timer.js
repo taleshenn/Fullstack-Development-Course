c0a1d7e2208dcd7b9c8cbb7b36d032aa6ba43c65
Tales Henn <taleshenn@gmail.com>
2023-02-13 14:38:23 -0300

    fix

--- m1s2/m1s2e5/timer.js
+++ m1s2/m1s2e5/timer.js
@@ -1,20 +1,19 @@
 let startTime = 10
 let recurence
 counter.innerHTML = "Click on the button to start"
-function countdown(){
-    if(startTime == 0){
+function countdown() {
+    if (startTime == 0) {
         counter.innerHTML = "Seu tempo acabou!<br/>Tente novamente!"
         console.log('Seu tempo acabou!! Tente novamente!!')
         clearTimeout(recurence)
     }
-    else{
+    else {
         let counter = document.getElementById("counter")
-        counter.innerText = startTime --
+        counter.innerText = startTime--
         console.log(startTime)
-    }    
+    }
 }
-
-function restart(){
+function restart() {
     startTime = 10
     recurence = setInterval(countdown, 1000)
 }
