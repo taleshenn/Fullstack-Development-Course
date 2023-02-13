1497c4493e1ae8099151500e7490d06fe73bcb5a
Tales Henn <taleshenn@gmail.com>
2023-02-13 10:43:44 -0300

    fix and slyling

--- m1s2/m1s2e2/login.js
+++ m1s2/m1s2e2/login.js
@@ -1,10 +1,10 @@
-function signin() {
-    let username = document.getElementById("username").value
-    let password = document.getElementById("password").value
-    let login = document.getElementById("login")
+// function signin() {
+//     let username = document.getElementById("username").value
+//     let password = document.getElementById("password").value
+//     let login = document.getElementById("login")
     
-    if (username == "" && password == "") 
-    {
-        console.log(`Logado como ${username} e senha ${password}`)
-    }
-    login.addEventListener("click", function(signin)
+//     if (username == "" && password == "") 
+//     {
+//         console.log(`Logado como ${username} e senha ${password}`)
+//     }
+//     login.addEventListener("click", function(signin)
