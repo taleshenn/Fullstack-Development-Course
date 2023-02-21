453f84f4f04d43ebeb0fae0a97971e725474815e
Tales Henn <taleshenn@gmail.com>
2023-02-21 10:13:10 -0300

    done

--- m1s2/m1s2e2/login.js
+++ m1s2/m1s2e2/login.js
@@ -1,16 +1,16 @@
 function loginFake() {
+	// .value to get the data from the input username
+	let userFake = document.querySelector("#user").value;
+	// .value to call the function loginFake
+	let passwordFake = document.querySelector("#pass").value;
+	// event onclick to get the data from the input password
 
-    // .value to get the data from the input username
-    let userFake = document.getElementById("user").value;
-    // .value to call the function loginFake
-    let passwordFake = document.getElementById("pass").value;
-    // event onclick to get the data from the input password 
-
-    if (userFake == "admin" && passwordFake == "admin") {
-        alert(`Logged in as ${userFake} with the password ${passwordFake}`)
-    }
-    else {
-        alert("Incorrect credentials")
-    }
-
+	if (userFake == "admin" && passwordFake == "admin") {
+		addEventListener("submit", (event) => {
+			event.preventDefault();
+			alert(`Logged in as ${userFake} with the password ${passwordFake}`);
+		});
+	} else {
+		alert("Incorrect credentials");
+	}
 }
