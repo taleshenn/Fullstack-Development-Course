bfcd0f9da049acf8b021d2de2c8aa9aab55b2e6d
Tales Henn <taleshenn@gmail.com>
2023-02-26 20:38:27 -0300

    fix for show

---
+++ m1s2/m1s2e3/login.js
@@ -0,0 +1,18 @@
+function loginFake() {
+	// .value to get the data from the input username
+	let userFake = document.querySelector("#user").value;
+	// .value to call the function loginFake
+	let passwordFake = document.querySelector("#pass").value;
+	// event onclick to get the data from the input password
+
+	if (userFake == "admin" && passwordFake == "admin") {
+		addEventListener("submit", (event) => {
+			event.preventDefault();
+			//alert(`Logged in as ${userFake} with the password ${passwordFake}`);
+			console.log(`Logged in as ${userFake} with the password ${passwordFake}`);
+		});
+	} else {
+		event.preventDefault();
+		console.log("Incorrect credentials");
+	}
+}
