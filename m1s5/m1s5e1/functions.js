c48725c4ec89e4c10bb1abe896e2ac4e367c587d
Tales Henn <taleshenn@gmail.com>
2023-04-10 17:29:33 -0300

    fix

--- m1s5/m1s5e1/functions.js
+++ m1s5/m1s5e1/functions.js
@@ -1,107 +1,26 @@
-// console.log("hello");
+const list = [
+	"Pedro",
+	"José",
+	"Aderbal",
+	"Danilo",
+	"Luisa",
+	"Vitoria",
+	"Luis",
+	"Danilo",
+	"José",
+];
 
-class Pessoa {
-	constructor(nome, idade) {
-		// <== atributos
-		this.nome = nome;
-		this.idade = idade;
-	}
-	falar() {
-		console.log("hello there");
-	}
-}
-let aluno = new Pessoa("tales", 47);
-aluno.falar();
+const filteredList = [];
+const duplicatedNames = [];
 
-class Pessoa {
-	constructor(nome, idade) {
-		this.nome = nome;
-		this.idade = idade;
-	}
-
-	falar() {
-		console.log("olá mundo");
+for (let i = 0; i < list.length; i++) {
+	if (filteredList.indexOf(list[i]) === -1) {
+		filteredList.push(list[i]);
+	} else {
+		duplicatedNames.push(list[i]);
 	}
 }
 
-// class Pessoa {
-//     constructor(nome, idade) {
-//       this.nome = nome;
-//       this.idade = idade;
-//     }
-
-//     falar() {
-//       console.log("olá mundo");
-//     }
-//   }
-
-//   class Professor extends Pessoa {
-//     constructor(nome, idade, materia, falar) {
-//       super(nome, idade, falar);
-//       this.materia = materia;
-//       this.nome = 'walberson'
-//     }
-//     darAula() {
-//       console.log("agora vamos dar aula de " + this.materia);
-//     }
-//     falar(){
-//       console.log("OLA MUNDOOOOOOOO!!!!");
-//     }
-//   }
-//   const walberson = new Professor("william", 18, "geografia");
-//   console.log(walberson.nome);
-///////// abastracao
-// class Parafuso {
-//   constructor(){
-//     this.name = 'tales'
-//     if(this.constructor === Parafuso){
-//       throw new Error('Classe abstrata, não pode ser instanciada')
-//     }
-//   }
-//   get tipo() {
-//     throw new Error ('Método abstrato, precisa ser implementado')
-//   }
-// }
-
-// class Fenda extends Parafuso {
-//   constructor() {
-//     super()
-//   }
-//   get tipo(){
-//     return 'fenda'
-//   }
-// }
-
-// class Philips extends Parafuso {
-//   constructor() {
-//     super()
-//   }
-//   get tipo(){
-//     return 'philips'
-//   }
-// }
-// class Allen extends Parafuso {
-
-// }
-// let parafuso = new Parafuso()
-// console.log(parafuso)
-// let allen = new Allen()
-
-// console.log(allen.tipo)
-
-////static
-// class Tipo {
-//   constructor (altura,largura){
-//     this.altura = altura
-//     this.largura = largura
-//   }
-//   static multiply (altura=this.altura,largura=this.largura){
-//     this.altura = altura
-//     this.largura = largura
-//     return this.altura * this.largura
-//   }
-// }
-
-// const multi = new Tipo(2,2)
-// //console.log(multi.multiply(2,2))
-// console.log(Tipo.multiply(2,2))
+console.log("Lista original: " + list);
+console.log("Lista sem duplicados: " + filteredList.join(", "));
+console.log("Nomes duplicados: " + duplicatedNames.join(", "));
