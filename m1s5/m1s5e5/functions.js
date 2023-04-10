5153e633fe2cb34d68e6663c4fa76844634a6467
Tales Henn <taleshenn@gmail.com>
2023-04-10 17:50:25 -0300

    fix

--- m1s5/m1s5e5/functions.js
+++ m1s5/m1s5e5/functions.js
@@ -1,107 +1,34 @@
-// console.log("hello");
-
-class Pessoa {
-	constructor(nome, idade) {
-		// <== atributos
-		this.nome = nome;
-		this.idade = idade;
+function contarVogais(str) {
+	// é uma string?
+	if (typeof str !== "string") {
+		return "Erro: O valor informado não é uma string.";
 	}
-	falar() {
-		console.log("hello there");
+
+	// Converter a string para letras minúsculas para facilitar a comparação
+	str = str.toLowerCase();
+
+	// Definir as vogais
+	const vogais = "aeiou";
+
+	let totalVogais = 0;
+
+	// Iterar sobre cada letra da string
+	for (let i = 0; i < str.length; i++) {
+		// Verificar se a letra atual é uma vogal
+		if (vogais.includes(str[i])) {
+			totalVogais++;
+		}
+	}
+
+	// Verificar se encontrou alguma vogal
+	if (totalVogais === 0) {
+		return "A palavra não contém nenhuma vogal.";
+	} else {
+		return `Total de vogais: ${totalVogais}`;
 	}
 }
-let aluno = new Pessoa("tales", 47);
-aluno.falar();
 
-class Pessoa {
-	constructor(nome, idade) {
-		this.nome = nome;
-		this.idade = idade;
-	}
-
-	falar() {
-		console.log("olá mundo");
-	}
-}
-
-// class Pessoa {
-//     constructor(nome, idade) {
-//       this.nome = nome;
-//       this.idade = idade;
-//     }
-
-//     falar() {
-//       console.log("olá mundo");
-//     }
-//   }
-
-//   class Professor extends Pessoa {
-//     constructor(nome, idade, materia, falar) {
-//       super(nome, idade, falar);
-//       this.materia = materia;
-//       this.nome = 'walberson'
-//     }
-//     darAula() {
-//       console.log("agora vamos dar aula de " + this.materia);
-//     }
-//     falar(){
-//       console.log("OLA MUNDOOOOOOOO!!!!");
-//     }
-//   }
-//   const walberson = new Professor("william", 18, "geografia");
-//   console.log(walberson.nome);
-///////// abastracao
-// class Parafuso {
-//   constructor(){
-//     this.name = 'tales'
-//     if(this.constructor === Parafuso){
-//       throw new Error('Classe abstrata, não pode ser instanciada')
-//     }
-//   }
-//   get tipo() {
-//     throw new Error ('Método abstrato, precisa ser implementado')
-//   }
-// }
-
-// class Fenda extends Parafuso {
-//   constructor() {
-//     super()
-//   }
-//   get tipo(){
-//     return 'fenda'
-//   }
-// }
-
-// class Philips extends Parafuso {
-//   constructor() {
-//     super()
-//   }
-//   get tipo(){
-//     return 'philips'
-//   }
-// }
-// class Allen extends Parafuso {
-
-// }
-// let parafuso = new Parafuso()
-// console.log(parafuso)
-// let allen = new Allen()
-
-// console.log(allen.tipo)
-
-////static
-// class Tipo {
-//   constructor (altura,largura){
-//     this.altura = altura
-//     this.largura = largura
-//   }
-//   static multiply (altura=this.altura,largura=this.largura){
-//     this.altura = altura
-//     this.largura = largura
-//     return this.altura * this.largura
-//   }
-// }
-
-// const multi = new Tipo(2,2)
-// //console.log(multi.multiply(2,2))
-// console.log(Tipo.multiply(2,2))
+// Exemplo de uso:
+const palavra = "exemplo de texto";
+const resultado = contarVogais(palavra);
+console.log(resultado);
