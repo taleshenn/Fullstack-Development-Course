b37ba2ba636575ef9386b0ca212528f3c98f5dc3
Tales Henn <taleshenn@gmail.com>
2023-04-10 17:45:38 -0300

    fix

--- m1s5/m1s5e4/functions.js
+++ m1s5/m1s5e4/functions.js
@@ -1,107 +1,31 @@
-// console.log("hello");
+const listaUsuarios = [
+	{ nome: "luis", idade: 26 },
+	{ nome: "norma", idade: 16 },
+	{ nome: "daiana", idade: 26 },
+	{ nome: "jorge", idade: 16 },
+	{ nome: "kauan", idade: 16 },
+	{ nome: "charles", idade: 26 },
+	{ nome: "marco", idade: 16 },
+	{ nome: "bruno", idade: 16 },
+];
 
-class Pessoa {
-	constructor(nome, idade) {
-		// <== atributos
-		this.nome = nome;
-		this.idade = idade;
-	}
-	falar() {
-		console.log("hello there");
-	}
+//  idades menores de 18
+const usuariosMenoresDe18 = listaUsuarios.filter(
+	(usuario) => usuario.idade < 18
+);
+
+//  idades maiores ou iguais a 18
+const usuariosMaioresDe18 = listaUsuarios.filter(
+	(usuario) => usuario.idade >= 18
+);
+
+// Exibir os resultados de forma amigável
+console.log("Usuários com idades menores de 18:");
+for (const usuario of usuariosMenoresDe18) {
+	console.log(`Nome: ${usuario.nome}, Idade: ${usuario.idade}`);
 }
-let aluno = new Pessoa("tales", 47);
-aluno.falar();
 
-class Pessoa {
-	constructor(nome, idade) {
-		this.nome = nome;
-		this.idade = idade;
-	}
-
-	falar() {
-		console.log("olá mundo");
-	}
+console.log("\nUsuários com idades maiores ou iguais a 18:");
+for (const usuario of usuariosMaioresDe18) {
+	console.log(`Nome: ${usuario.nome}, Idade: ${usuario.idade}`);
 }
-
-// class Pessoa {
-//     constructor(nome, idade) {
-//       this.nome = nome;
-//       this.idade = idade;
-//     }
-
-//     falar() {
-//       console.log("olá mundo");
-//     }
-//   }
-
-//   class Professor extends Pessoa {
-//     constructor(nome, idade, materia, falar) {
-//       super(nome, idade, falar);
-//       this.materia = materia;
-//       this.nome = 'walberson'
-//     }
-//     darAula() {
-//       console.log("agora vamos dar aula de " + this.materia);
-//     }
-//     falar(){
-//       console.log("OLA MUNDOOOOOOOO!!!!");
-//     }
-//   }
-//   const walberson = new Professor("william", 18, "geografia");
-//   console.log(walberson.nome);
-///////// abastracao
-// class Parafuso {
-//   constructor(){
-//     this.name = 'tales'
-//     if(this.constructor === Parafuso){
-//       throw new Error('Classe abstrata, não pode ser instanciada')
-//     }
-//   }
-//   get tipo() {
-//     throw new Error ('Método abstrato, precisa ser implementado')
-//   }
-// }
-
-// class Fenda extends Parafuso {
-//   constructor() {
-//     super()
-//   }
-//   get tipo(){
-//     return 'fenda'
-//   }
-// }
-
-// class Philips extends Parafuso {
-//   constructor() {
-//     super()
-//   }
-//   get tipo(){
-//     return 'philips'
-//   }
-// }
-// class Allen extends Parafuso {
-
-// }
-// let parafuso = new Parafuso()
-// console.log(parafuso)
-// let allen = new Allen()
-
-// console.log(allen.tipo)
-
-////static
-// class Tipo {
-//   constructor (altura,largura){
-//     this.altura = altura
-//     this.largura = largura
-//   }
-//   static multiply (altura=this.altura,largura=this.largura){
-//     this.altura = altura
-//     this.largura = largura
-//     return this.altura * this.largura
-//   }
-// }
-
-// const multi = new Tipo(2,2)
-// //console.log(multi.multiply(2,2))
-// console.log(Tipo.multiply(2,2))
