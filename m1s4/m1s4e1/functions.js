ef25adf4415c83da6631192965116ae647e36653
Tales Henn <taleshenn@gmail.com>
2023-04-10 16:28:11 -0300

    fix

--- m1s4/m1s4e1/functions.js
+++ m1s4/m1s4e1/functions.js
@@ -1,107 +1,25 @@
-// console.log("hello");
+class caixaRegistradora {
+	estoque = [];
 
-class Pessoa {
-	constructor(nome, idade) {
-		// <== atributos
-		this.nome = nome;
-		this.idade = idade;
-	}
-	falar() {
-		console.log("hello there");
-	}
-}
-let aluno = new Pessoa("tales", 47);
-aluno.falar();
+	constructor() {}
 
-class Pessoa {
-	constructor(nome, idade) {
-		this.nome = nome;
-		this.idade = idade;
+	adicionaEstoque(codigoBarras, preco, nome) {
+		if (!codigoBarras || !preco || !nome) {
+			console.log("Todos os campos são obrigatórios. Nada foi adicionado!");
+		} else {
+			this.estoque.push({ codigoBarras, preco, nome });
+		}
 	}
 
-	falar() {
-		console.log("olá mundo");
+	startShopping(name) {}
+
+	cart(codigoBarras, quantidade) {}
+
+	show() {
+		console.log(this.estoque);
 	}
 }
 
-// class Pessoa {
-//     constructor(nome, idade) {
-//       this.nome = nome;
-//       this.idade = idade;
-//     }
-
-//     falar() {
-//       console.log("olá mundo");
-//     }
-//   }
-
-//   class Professor extends Pessoa {
-//     constructor(nome, idade, materia, falar) {
-//       super(nome, idade, falar);
-//       this.materia = materia;
-//       this.nome = 'walberson'
-//     }
-//     darAula() {
-//       console.log("agora vamos dar aula de " + this.materia);
-//     }
-//     falar(){
-//       console.log("OLA MUNDOOOOOOOO!!!!");
-//     }
-//   }
-//   const walberson = new Professor("william", 18, "geografia");
-//   console.log(walberson.nome);
-///////// abastracao
-// class Parafuso {
-//   constructor(){
-//     this.name = 'tales'
-//     if(this.constructor === Parafuso){
-//       throw new Error('Classe abstrata, não pode ser instanciada')
-//     }
-//   }
-//   get tipo() {
-//     throw new Error ('Método abstrato, precisa ser implementado')
-//   }
-// }
-
-// class Fenda extends Parafuso {
-//   constructor() {
-//     super()
-//   }
-//   get tipo(){
-//     return 'fenda'
-//   }
-// }
-
-// class Philips extends Parafuso {
-//   constructor() {
-//     super()
-//   }
-//   get tipo(){
-//     return 'philips'
-//   }
-// }
-// class Allen extends Parafuso {
-
-// }
-// let parafuso = new Parafuso()
-// console.log(parafuso)
-// let allen = new Allen()
-
-// console.log(allen.tipo)
-
-////static
-// class Tipo {
-//   constructor (altura,largura){
-//     this.altura = altura
-//     this.largura = largura
-//   }
-//   static multiply (altura=this.altura,largura=this.largura){
-//     this.altura = altura
-//     this.largura = largura
-//     return this.altura * this.largura
-//   }
-// }
-
-// const multi = new Tipo(2,2)
-// //console.log(multi.multiply(2,2))
-// console.log(Tipo.multiply(2,2))
+const caixa = new caixaRegistradora();
+caixa.addStock("#888888", "12.34", "produto qualquer");
+caixa.show();
